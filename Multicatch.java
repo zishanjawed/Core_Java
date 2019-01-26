83aee88285556ef80478ac042903b00da9a0c875
ZISHAN JAWED <32324041+zishanjawed@users.noreply.github.com>
2019-01-26 18:54:02 +0530

    Add files via upload

--- Multicatch.java
+++ Multicatch.java
@@ -1,5 +1,18 @@
 package com.Zishan.c1;
 
 public class Multicatch {
+	public static void main(String ... a)
+	{
+		int [] i = new int[3];
+		try{
+			//System.out.println(1/0);
+			System.out.println(i[5]);
+		}
+		catch(ArithmeticException | ArrayIndexOutOfBoundsException  e )
+		{
+			System.out.print(e);
+		}
+	}
+	
 
 }
