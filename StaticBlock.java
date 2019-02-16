679636c7ca85312e35ac3d998495847e65e84ee4
ZISHAN JAWED <32324041+zishanjawed@users.noreply.github.com>
2019-02-16 23:00:02 +0530

    Add files via upload
    Implementation of static block

---
+++ StaticBlock.java
@@ -0,0 +1,19 @@
+package com.zishan.video70;
+
+public class StaticBlock {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi, From main");
+	}
+	static{
+		System.out.println("Bye, From static 3");
+	}
+	static {
+		System.out.println("Hello from static 1");
+	}
+	static{
+		System.out.println("This is static 2");
+	}
+	
+
+}
