1b488e4b8df489b5b56d59714e55eed65713f3e2
ZISHAN JAWED <32324041+zishanjawed@users.noreply.github.com>
2019-02-16 23:27:34 +0530

    Importing static member and method of another class
    In this core Java program I have import static member and static method of AboutStatic calss
    reside into the different package into the ImportingStatic class

---
+++ AboutStatic.java
@@ -0,0 +1,9 @@
+package com.zishan.video70;
+
+public class AboutStatic {
+	static public int a =10;
+	static public void show()
+	{
+		System.out.println("Inside the Static method");
+	}
+}
---
+++ ImportingStatic.java
@@ -0,0 +1,10 @@
+import static com.zishan.video70.AboutStatic.*;
+
+import com.zishan.video70.AboutStatic;
+public class ImportingStatic {
+	public static void main(String[] args) {
+	System.out.println("The value of static veriable a = "+a);
+	AboutStatic.show();
+	
+	}
+}
